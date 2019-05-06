bdb0a94631132be9f4e37131010beddb190ee32e
Sandy Zuleta <sandyzuleta@gmail.com>
2019-05-06 16:32:27 -0300

    primer commit

---
+++ precache-manifest.a523a6fed9327191f91a3cf18f3922a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777e0bc6665c4f69da3aec9f69104bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9c7fcbb99c81bf7cb0",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6a9c7fcbb99c81bf7cb0",
+    "url": "/static/js/main.af8678f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
